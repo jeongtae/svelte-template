e00c0102a7d453ef3b8809658611119da199c43e
Jeongtae Kim <rsatang5@naver.com>
2021-03-15 15:29:14 +0900

    Fix TypeScript issue on Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,13 @@
+const sveltePreprocess = require("svelte-preprocess");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|ts)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|ts)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: config => {
+    const svelteLoader = config.module.rules.find(r => r.loader?.includes("svelte-loader"));
+    svelteLoader.options.preprocess = sveltePreprocess({
+      typescript: { transpileOnly: true },
+    });
+    return config;
+  },
+};
