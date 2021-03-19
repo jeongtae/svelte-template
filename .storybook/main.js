5a6d2180e2bfc51b747c9e7a990061249b56b278
Jeongtae Kim <rsatang5@naver.com>
2021-03-19 15:10:36 +0900

    Re-order addons of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const sveltePreprocess = require("svelte-preprocess");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|ts)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-links"],
   webpackFinal: config => {
     const svelteLoader = config.module.rules.find(r => r.loader?.includes("svelte-loader"));
     svelteLoader.options.preprocess = sveltePreprocess({
